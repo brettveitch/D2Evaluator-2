73367830d04fb986175bd875784b60efe8f1e60b
brettveitch <blveitch@comcast.net>
2023-04-16 11:55:41 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,22 @@
+from helperCommands import *
+from inputHelpers import getCommand
+
+def runProgram():
+    while(True):
+        command = getCommand()
+        if command == "q":
+            break
+        if command == "c":
+            clear()
+        if command == "e":
+            evaluate()
+        if command == "h":
+            helpMenu()
+        if command == "f":
+            find()
+        if command == "i":
+            invalid()
+        if command == "t":
+            trim()
+
+runProgram()
